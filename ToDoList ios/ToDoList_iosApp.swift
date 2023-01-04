1b4af1f090a008e7f1f18bbf8fd3cb2cf47c59b6
ismeniabrandao <brandaoismenia@gmail.com>
2023-01-04 04:30:36 +0100

    Initial Commit

---
+++ ToDoList ios/ToDoList_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_iosApp.swift
+//  ToDoList ios
+//
+//  Created by Ismenia Brandao on 2023-01-04.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
